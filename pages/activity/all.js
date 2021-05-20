c292c0ff4ef0a0174c806f5f7431792d0b89eaba
gexiaowei <gandxiaowei@gmail.com>
2021-05-20 12:25:24 +0800

    fix 展会服务.tif

--- pages/activity/all.js
+++ pages/activity/all.js
@@ -1,27 +1,32 @@
 import util from '../../utils/util.js'
 
-const { globalData, globalData: { http, expoId, regeneratorRuntime } } = getApp()
+const {
+  globalData,
+  globalData: { http, expoId, regeneratorRuntime }
+} = getApp()
 
 Page({
-    data: {
-        schedule_list: []
-    },
+  data: {
+    schedule_list: []
+  },
 
-    onLoad: function(options) {
-        this.getScheduleList()
-    },
+  onLoad: function (options) {
+    this.getScheduleList()
+  },
 
-    getScheduleList: async function() {
-        const { data: { status, result = [], message } } = await wx.pro.request({
-            url: `${http}/schedule/listSchedule`,
-            method: 'GET',
-            data: {
-                expoId,
-                num: 200,
-                bbs: 1
-            }
-        })
+  getScheduleList: async function () {
+    const {
+      data: { status, result = [], message }
+    } = await wx.pro.request({
+      url: `${http}/schedule/listSchedule`,
+      method: 'GET',
+      data: {
+        expoId,
+        num: 200,
+        bbs: 1
+      }
+    })
 
-        this.setData({ schedule_list: util.groupBy(result, 'sTime') })
-    },
+    this.setData({ schedule_list: util.groupBy(result, 'sTime') })
+  }
 })
