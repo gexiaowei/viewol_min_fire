2ab08f57661da961031da380331984eaf8237254
gexiaowei <gandxiaowei@gmail.com>
2023-08-04 17:52:38 +0800

    fix sign up url

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-import regeneratorRuntime from '/utils/wxPromise.min.js'
+import regeneratorRuntime from "/utils/wxPromise.min.js";
 //app.js
 App({
   onLaunch: function (options) {
-    const user_id = wx.getStorageSync('uid')
+    const user_id = wx.getStorageSync("uid");
     if (user_id && user_id > 0) {
-      this.globalData.uid = user_id
+      this.globalData.uid = user_id;
     }
-    const { windowWidth } = wx.getSystemInfoSync()
-    this.globalData.pixelRatio = 750 / windowWidth
+    const { windowWidth } = wx.getSystemInfoSync();
+    this.globalData.pixelRatio = 750 / windowWidth;
   },
   globalData: {
-    honesty_json: 'https://www.view-ol.com/cxqy.json',
+    honesty_json: "https://www.view-ol.com/cxqy.json",
     sign_up_url:
-      'https://service.ciec.com.cn/ciec_sw/modules/register/preRegm/92072/wechat/zh/n',
-    video_url: 'https://20210527.15shang.club',
+      "https://service.ciec.com.cn/ciec_sw/modules/register/preRegm/92226/wechat/zh/n",
+    video_url: "https://20210527.15shang.club",
     userInfo: null,
-    http: 'https://www.view-ol.com/viewol_web',
-    web_http: 'https://www.view-ol.com/',
+    http: "https://www.view-ol.com/viewol_web",
+    web_http: "https://www.view-ol.com/",
 
     encryptedData: null,
     openid: null,
@@ -26,6 +26,6 @@ App({
     userJoin: 1,
     firefighting_exhibitors_award: 0,
     firefighting_activity_self: 0,
-    regeneratorRuntime
-  }
-})
+    regeneratorRuntime,
+  },
+});
